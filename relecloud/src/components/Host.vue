bb497fe4c8aee97d4fd9f89dbd4f8b2addb934bc
sa999na <lumimosita999@icloud.com>
2022-08-02 18:03:43 +0900

    propsの追加

---
+++ relecloud/src/components/Host.vue
@@ -0,0 +1,84 @@
+<template>
+<section>
+<div class="nav-bar"></div>
+<h1>Relecloud Galaxy Tours</h1>
+
+<div>
+    <h2>{{ cruise.name }}</h2>
+    <div>{{ cruise.description }}</div>
+    <hr />
+
+    <div class="row">
+        <div>
+            <!-- TODO: Add booking-form -->
+            <booking-list :bookings="bookings"></booking-list>
+        </div>
+        <div>
+            <!-- TODO: Add booking-list -->
+
+        </div>
+    </div>
+</div>
+</section>
+</template>
+
+<script>
+import BookingList from './BookingList.vue'
+
+export default {
+  name: "Host",
+  data() {
+    return {
+      cruise: {
+        name: "Cuise to the moon",
+        description:
+          "Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.",
+        cabins: [
+          { name: "Coach", price: 125000 },
+          { name: "Business", price: 275000 },
+          { name: "First", price: 430000 },
+        ],
+      },
+      bookings: [{ name: "Sample", price: 0 }
+      ]
+    }
+  },
+  //TODO: add components
+  components: {
+    BookingList,
+
+},
+
+  //TODO: add methods
+};
+</script>
+
+<style>
+body {
+    background-color: #f2f2f2;
+    margin: 0, 5%;
+    font-family: tahoma;
+}
+
+.row {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    vertical-align: middle;
+    margin: 2em;
+}
+
+.button {
+    background-color: #39495c;
+    border-radius: 5px;
+    color: white;
+    text-align: center;
+}
+
+.nav-bar {
+    background: linear-gradient(-50deg, #010801, #0d0d60);
+    height: 60px;
+    margin-bottom: 25px;
+}
+</style>
+
+  
--- relecloud/src/main.js
+++ relecloud/src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-d
-createApp(App).mount('#app')
+import Host from './components/Host.vue'
+
+createApp(Host).mount('#app')
